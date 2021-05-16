e66af1e150c451a3c84f6019a7ba8089ce4763d1
SandraT <sndrbltr@gmail.com>
2021-05-16 10:49:41 +0000

    Age difference other solution

--- js/age-difference.js
+++ js/age-difference.js
@@ -22,6 +22,23 @@ function differenceInAges(ages) {
     return [min, max, difference];                                          
 }
 
+/* ARBA
+function differenceInAges(ages) {
+    let youngest = Infinity;  
+    let oldest = 0;    // g.b. -Inginity, bet logiskai neigiamo amziaus nera
+
+    for (let i = 0; i < ages.lengt; i++) {                               
+        if (ages[i] < youngest) {
+            youngest = ages[i];
+
+        }   
+        if (ages[i] > oldest) {
+            oldest = ages[i];
+        }
+    }
+    return [youngest, oldest, oldest-youngest]
+}
+*/
 
 console.log(differenceInAges([82, 15, 6, 38, 35]), '->', [6, 82, 76]);
 console.log(differenceInAges([57, 99, 14, 32]), '->', [14, 99, 85]);
